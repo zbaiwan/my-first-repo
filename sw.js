4e48b28f2ba01d4f082e2a31c363bad45de5953b
zbaiwan <42604131+zbaiwan@users.noreply.github.com>
2018-11-22 14:55:51 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
  'use strict';
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+importScripts("sw-toolbox.js");
 toolbox.precache(["index.html","main.css","youhun.jpg"]);
 
 toolbox.router.get('/images/*', toolbox.cacheFirst);
